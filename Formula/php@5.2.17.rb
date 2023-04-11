cd878f7c5d4e7c5515d76d99bb0204d9bcfa4026
vvtommy <vvtommy@gmail.com>
2023-04-11 12:10:21 +0800

    to homebrew 2

--- Formula/php@5.2.17.rb
+++ Formula/php@5.2.17.rb
@@ -1,161 +1,148 @@
-require 'formula'
-
-def mysql_installed?
-  `which mysql_config`.length > 0
-end
-
 class PhpAT5217 < Formula
-  url 'https://museum.php.net/php5/php-5.2.17.tar.gz'
-  homepage ''
-  sha256 '1abe07c1fdd64184708a3ba179abcfcca5662a4e0d2037eb2748b75abc42e767'
-  version '5.2.17'
+  desc "PHP 5.2.17"
+  homepage ""
+  url "https://museum.php.net/php5/php-5.2.17.tar.gz"
+  sha256 "1abe07c1fdd64184708a3ba179abcfcca5662a4e0d2037eb2748b75abc42e767"
 
-  skip_clean [ 'bin', 'sbin' ]
+  depends_on "gettext"
+  depends_on "icu4c" if build.with? "intl"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libxml2"
+  depends_on "mcrypt"
+  depends_on "postgresql" if build.with? "pgsql"
+  depends_on "readline" if build.with? "readline"
 
-  depends_on 'libxml2'
-  depends_on 'jpeg'
-  depends_on 'libpng'
-  depends_on 'mcrypt'
-  depends_on 'gettext'
-  if ARGV.include? '--with-mysql'
-    depends_on 'mysql' => :recommended unless mysql_installed?
-  end
-  if ARGV.include? '--with-fpm'
-    depends_on 'libevent'
-  end
-  if ARGV.include? '--with-pgsql'
-    depends_on 'postgresql'
-  end
-  if ARGV.include? '--with-mssql'
-    depends_on 'freetds'
-  end
-  if ARGV.include? '--with-intl'
-    depends_on 'icu4c'
-  end
-  if ARGV.include? '--with-readline'
-    depends_on 'readline'
-  end
-
-  # depends_on 'cmake'
-
-  def configure_args
-    args = [
-      "--prefix=#{prefix}",
-      "--disable-debug",
-      "--disable-dependency-tracking",
-      "--with-config-file-path=#{prefix}/etc",
-      "--with-iconv-dir=/usr",
-      "--enable-soap",
-      "--enable-sqlite-utf8",
-      "--enable-sockets",
-      "--enable-fastcgi",
-      "--enable-memory-limit",
-      "--enable-mbstring",
-      "--enable-mbregex",
-      "--enable-zend-multibyte",
-      "--with-openssl=/usr",
-      "--with-zlib=/usr",
-      "--with-bz2=/usr",
-      "--with-xmlrpc",
-      "--with-libxml-dir=#{Formula.factory('libxml2').prefix}",
-      "--with-xsl=/usr",
-      "--with-curl=/usr",
-      "--with-gd",
-      "--with-mssql",
-      "--with-mysql",
-      "--with-readline",
-      "--enable-gd-native-ttf",
-      "--with-mcrypt=#{Formula.factory('mcrypt').prefix}",
-      "--with-jpeg-dir=#{Formula.factory('jpeg').prefix}",
-      "--with-png-dir=#{Formula.factory('libpng').prefix}",
-      "--with-gettext=#{Formula.factory('gettext').prefix}",
-      "--mandir=#{man}"
-    ]
-
-    # Free type support
-    if File.exist? "/usr/X11"
-      args.push "--with-freetype-dir=/usr/X11"
-    end
-
-    # Bail if both php-fpm and apxs are enabled
-    # http://bugs.php.net/bug.php?id=52419
-    if (ARGV.include? '--with-fpm') && (ARGV.include? '--with-apache')
-      onoe "You can only enable PHP FPM or Apache, not both"
-      puts "http://bugs.php.net/bug.php?id=52419"
-      exit 99
-    end
-
-    # Enable PHP FPM
-    if ARGV.include? '--with-fpm'
-      args.push "--enable-fpm"
-    end
-
-    # Build Apache module
-    if ARGV.include? '--with-apache'
-      args.push "--with-apxs2=/usr/sbin/apxs"
-      args.push "--libexecdir=#{prefix}/libexec"
-    end
-
-    if ARGV.include? '--with-mysql'
-      args.push "--with-mysql-sock=/tmp/mysql.sock"
-      args.push "--with-mysqli"
-      args.push "--with-mysql=/usr/local"
-      args.push "--with-pdo-mysql=/usr/local"
-    end
-
-    if ARGV.include? '--with-pgsql'
-      args.push "--with-pgsql=#{Formula.factory('postgresql').prefix}"
-      args.push "--with-pdo-pgsql=#{Formula.factory('postgresql').prefix}"
-    end
-
-    if ARGV.include? '--with-intl'
-      args.push "--enable-intl"
-      args.push "--with-icu-dir=#{Formula.factory('icu4c').prefix}"
-    end
-    
-    if ARGV.include? '--with-readline'
-      args.push "--with-readline=#{Formula.factory('readline').prefix}"
-    end
-
-    return args
-  end
+  uses_from_macos "curl"
+  uses_from_macos "freetds"
+  uses_from_macos "libxslt"
+  uses_from_macos "openldap"
+  uses_from_macos "sqlite"
+  uses_from_macos "unixodbc"
 
   def install
-    # Because for icu4c, we must link with c++ when building with intl extension
-    ENV.append 'LDFLAGS', '-lstdc++' if ARGV.include? '--with-intl'
+    args = %W[
+      --prefix=#{prefix}
+      --disable-debug
+      --disable-dependency-tracking
+      --with-config-file-path=#{etc}
+      --with-iconv-dir=/usr
+      --with-libxml-dir=#{Formula["libxml2"].opt_prefix}
+      --with-openssl=/usr
+      --with-zlib=/usr
+      --enable-fastcgi
+      --enable-mbregex
+      --enable-mbstring
+      --enable-memory-limit
+      --enable-soap
+      --enable-sockets
+      --enable-sqlite-utf8
+      --enable-zend-multibyte
+      --mandir=#{man}
+      --with-bz2=/usr
+      --with-curl=/usr
+      --with-freetype-dir=/usr/local/opt/freetype
+      --with-gd
+      --with-gettext=#{Formula["gettext"].opt_prefix}
+      --with-jpeg-dir=#{Formula["jpeg"].opt_prefix}
+      --with-mcrypt=#{Formula["mcrypt"].opt_prefix}
+      --with-mssql=#{Formula["freetds"].opt_prefix}
+      --with-png-dir=#{Formula["libpng"].opt_prefix}
+      --with-readline=#{Formula["readline"].opt_prefix}
+      --with-xmlrpc
+      --with-xsl=/usr
+    ]
 
-    system "./configure", *configure_args
+    args << "--enable-intl" if build.with? "intl"
+    args << "--with-apxs2=/usr/sbin/apxs" if build.with? "apache"
+    args << "--with-fpm" if build.with? "fpm"
+    args << "--with-mysqli=#{Formula["mysql"].opt_bin}/mysql_config" if build.with? "mysql"
+    args << "--with-mysql-sock=/tmp/mysql.sock" if File.exist?("/tmp/mysql.sock")
+    args << "--with-pdo-mysql=#{Formula["mysql"].opt_bin}/mysql_config" if build.with? "mysql"
+    args << "--with-pdo-pgsql=#{Formula["postgresql"].opt_bin}/pg_config" if build.with? "pgsql"
+    args << "--with-pgsql=#{Formula["postgresql"].opt_bin}/pg_config" if build.with? "pgsql"
 
-    if ARGV.include? '--with-apache'
-      # Use Homebrew prefix for the Apache libexec folder
-      inreplace "Makefile",
-        "INSTALL_IT = $(mkinstalldirs) '$(INSTALL_ROOT)/usr/libexec/apache2' && $(mkinstalldirs) '$(INSTALL_ROOT)/private/etc/apache2' && /usr/sbin/apxs -S LIBEXECDIR='$(INSTALL_ROOT)/usr/libexec/apache2' -S SYSCONFDIR='$(INSTALL_ROOT)/private/etc/apache2' -i -a -n php5 libs/libphp5.so",
-        "INSTALL_IT = $(mkinstalldirs) '#{prefix}/libexec/apache2' && $(mkinstalldirs) '$(INSTALL_ROOT)/private/etc/apache2' && /usr/sbin/apxs -S LIBEXECDIR='#{prefix}/libexec/apache2' -S SYSCONFDIR='$(INSTALL_ROOT)/private/etc/apache2' -i -a -n php5 libs/libphp5.so"
-    end
-    
+    system "./configure", *args
     system "make"
-    system "make install"
+    system "make", "install"
 
-    system "cp ./php.ini-recommended #{prefix}/etc/php.ini"
-
-    if ARGV.include? '--with-fpm'
-      (prefix+'org.php.php-fpm.plist').write startup_plist
-      system "cp #{prefix}/etc/php-fpm.conf.default #{prefix}/etc/php-fpm.conf"
-      (prefix+'var/log').mkpath
-      touch prefix+'var/log/php-fpm.log'
+    if build.with? "fpm"
+      (prefix/"org.php.php-fpm.plist").write startup_plist
+      cp "#{etc}/php-fpm.conf.default", "#{etc}/php-fpm.conf"
+      (var/"log").mkpath
+      touch var/"log/php-fpm.log"
     end
   end
 
- def caveats; <<-EOS
-   For 10.5 and Apache:
-    Apache needs to run in 32-bit mode. You can either force Apache to start 
-    in 32-bit mode or you can thin the Apache executable.
-   
-   To enable PHP in Apache add the following to httpd.conf and restart Apache:
-    LoadModule php5_module    #{prefix}/libexec/apache2/libphp5.so
+  def configure_cellar_paths; end
 
-    The php.ini file can be found in:
-      #{prefix}/etc/php.ini
-   EOS
- end
+  def post_install
+    # Make sure the Web server will find the PHP module
+    if build.with? "apache"
+      apache_conf = <<~EOS
+        LoadModule php5_module    #{libexec}/apache2/libphp5.so
+      EOS
+      File.open("/etc/apache2/other/php5_module.conf", "w") { |f| f.write apache_conf }
+    end
+  end
+
+  def caveats
+    s = <<-EOS
+      For 10.5 and Apache:
+        Apache needs to run in 32-bit mode. You can either force Apache to start
+        in 32-bit mode or you can thin the Apache executable.
+
+      To enable PHP in Apache add the following to httpd.conf and restart Apache:
+        LoadModule php5_module    #{libexec}/apache2/libphp5.so
+
+      The php.ini file can be found in:
+        #{etc}/php.ini
+    EOS
+
+    if build.with? "fpm"
+      s += <<~EOS
+
+        To launch php-fpm on startup:
+          * If this is your first install:
+              sudo cp #{opt_prefix}/org.php.php-fpm.plist /Library/LaunchDaemons/
+              sudo launchctl load -w /Library/LaunchDaemons/org.php.php-fpm.plist
+
+          * If this is an upgrade and you already have the org.php.php-fpm.plist loaded:
+              sudo launchctl unload -w /Library/LaunchDaemons/org.php.php-fpm.plist
+              sudo cp #{opt_prefix}/org.php.php-fpm.plist /Library/LaunchDaemons/
+              sudo launchctl load -w /Library/LaunchDaemons/org.php.php-fpm.plist
+      EOS
+    end
+
+    s
+  end
+
+  def startup_plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+        "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>org.php.php-fpm</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_sbin}/php-fpm</string>
+          <string>--fpm-config</string>
+          <string>#{etc}/php-fpm.conf</string>
+        </array>
+        <key>KeepAlive</key>
+        <true/>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    output = shell_output("#{bin}/php -n -r 'echo \"Hello World\n\";'")
+    assert_equal "Hello World\n", output
+  end
 end
